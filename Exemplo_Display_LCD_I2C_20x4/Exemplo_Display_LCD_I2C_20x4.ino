586684317c67f9ce599350d19ae2f56365b109b8
jairogp <jairogp@users.noreply.github.com>
2017-05-07 05:27:20 -0300

    Add files via upload

---
+++ Exemplo_Display_LCD_I2C_20x4/Exemplo_Display_LCD_I2C_20x4.ino
@@ -0,0 +1,31 @@
+    /*
+    Pinagem do LCD
+    
+    GND => (-)
+    VCC => 5V
+    SDA => A4
+    SCL => A5 
+    */
+    
+   #include <Wire.h> 
+   #include <LiquidCrystal_I2C.h>
+
+   LiquidCrystal_I2C lcd(0x3F,20,4);  
+
+   void setup()
+   {
+   lcd.init();           // initializa o lcd 
+   lcd.backlight();      // acende a luz de fundo no lcd
+   lcd.setCursor(8,0);
+   lcd.print("Teste");
+   lcd.setCursor(7,1);
+   lcd.print("Escrita");
+   lcd.setCursor(9,2);
+   lcd.print("com");
+   lcd.setCursor(7,3);
+   lcd.print("LCD 20x4");
+   }
+
+   void loop()
+   {
+   }
